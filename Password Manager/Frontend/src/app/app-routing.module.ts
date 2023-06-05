251e5d4ffb92a416fc837793a4b6956bf92d97b1
dutualincalin <57103507+dutualincalin@users.noreply.github.com>
2023-06-06 01:04:47 +0300

    Finished welcome page

--- Password Manager/Frontend/src/app/app-routing.module.ts
+++ Password Manager/Frontend/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {WelcomePageComponent} from "./components/pages/welcome-page/welcome-page
 import {HomePageComponent} from "./components/pages/home-page/home-page.component";
 import {CreatePasswordPageComponent} from "./components/pages/create-password-page/create-password-page.component";
 import {QrPageComponent} from "./components/pages/qr-page/qr-page.component";
-import {NotFoundPageComponent} from "./components/pages/not-found-page/not-found-page.component";
 
 const routes: Routes = [
   {path: 'home', component:HomePageComponent},
